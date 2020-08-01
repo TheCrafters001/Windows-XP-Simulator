49bf28cbb411e61231d2bc3d5c81454f97d23431
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-08-01 11:24:04 -0400

    Update Icons To Be From WinXP

--- src/Windows XP Simulator/Form1.Designer.vb
+++ src/Windows XP Simulator/Form1.Designer.vb
@@ -28,8 +28,11 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(444, 276)
+        Me.ClientSize = New System.Drawing.Size(410, 332)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
