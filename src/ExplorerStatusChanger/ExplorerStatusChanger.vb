488a2267dba361ebe8ace7899786328769156b72
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-07-31 22:57:56 -0400

    Initial Upload

---
+++ src/ExplorerStatusChanger/ExplorerStatusChanger.vb
@@ -0,0 +1,8 @@
+﻿Public Class ExplorerStatusChanger
+    Public Shared Sub KillExplorer()
+        Shell("taskkill /f /im explorer.exe")
+    End Sub
+    Public Shared Sub StartExplorer()
+        Shell("explorer.exe")
+    End Sub
+End Class
---
+++ src/Windows XP Simulator/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ms As New System.IO.MemoryStream(My.Resources.arrow_m)
+        Me.Cursor = New Cursor(ms)
+    End Sub
+End Class
